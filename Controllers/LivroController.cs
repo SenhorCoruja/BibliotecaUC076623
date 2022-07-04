a4ff17bae5fd94905f9aca22c46106515703ec3d
Marcio Arruda <marcioarruda250@gmail.com>
2022-07-04 01:08:30 -0300

    Cadastro de Livro - Correção dos Campos Obrigatórios

--- Controllers/LivroController.cs
+++ Controllers/LivroController.cs
@@ -13,19 +13,24 @@ namespace Biblioteca.Controllers
 
         [HttpPost]
         public IActionResult Cadastro(Livro l)
-        {
-            LivroService livroService = new LivroService();
-
-            if(l.Id == 0)
-            {
-                livroService.Inserir(l);
+        {   
+            if (l.Autor == "" || l.Ano==0 || l.Titulo== ""){
+                
             }
-            else
-            {
-                livroService.Atualizar(l);
-            }
+            else{
+                LivroService livroService = new LivroService();
 
+                if(l.Id == 0)
+                {
+                    livroService.Inserir(l);
+                }
+                else
+                {
+                    livroService.Atualizar(l);
+                }
+            }
             return RedirectToAction("Listagem");
+            
         }
 
         public IActionResult Listagem(string tipoFiltro, string filtro)
