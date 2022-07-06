9d2c81da1bf917485ecf388f717520175b4c72bb
Marcio Arruda <marcioarruda250@gmail.com>
2022-07-06 00:54:38 -0300

    Empréstimo - Checagem de Login

--- Controllers/EmprestimoController.cs
+++ Controllers/EmprestimoController.cs
@@ -10,7 +10,8 @@ namespace Biblioteca.Controllers
     public class EmprestimoController : Controller
     {
         public IActionResult Cadastro()
-        {
+        {   
+            Autenticacao.CheckLogin(this);
             LivroService livroService = new LivroService();
             EmprestimoService emprestimoService = new EmprestimoService();
 
@@ -42,6 +43,7 @@ namespace Biblioteca.Controllers
 
         public IActionResult Listagem(string tipoFiltro, string filtro)
         {
+            Autenticacao.CheckLogin(this);
             FiltrosEmprestimos objFiltro = null;
             if(!string.IsNullOrEmpty(filtro))
             {
@@ -55,6 +57,7 @@ namespace Biblioteca.Controllers
 
         public IActionResult Edicao(int id)
         {
+            Autenticacao.CheckLogin(this);
             LivroService livroService = new LivroService();
             EmprestimoService em = new EmprestimoService();
             Emprestimo e = em.ObterPorId(id);
